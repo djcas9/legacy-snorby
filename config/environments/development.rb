eb3bae1d68e895cdb23686c97cd99b697877c8f1
Dustin <dustin.webber@gmail.com>
2009-06-29 23:26:45 -0500

    working on reports

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,10 +17,3 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = true
 
 ActionMailer::Base.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address        => "smtp.gmail.com",
-  :port           => 587,
-  :authentication => :plain,
-  :user_name      => "dustin.webber@gmail.com",
-  :password       => "Moscow4Life" 
-}
