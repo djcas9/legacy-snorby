79449f4e09ea37f9b9679ff2ea50987a5d255651
Dustin <dustin.webber@gmail.com>
2009-07-08 00:30:33 -0500

    added clippy - github like copy/clipboard for ips

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -82,5 +82,33 @@ module ApplicationHelper
       "Snorbby Needs To be Restarted."
     end
   end
+  
+  
+  def clippy(text, bgcolor='#FFFFFF')
+    html = <<-EOF
+      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+              width="110"
+              height="12"
+              id="clippy" >
+      <param name="movie" value="/flash/clippy.swf"/>
+      <param name="allowScriptAccess" value="always" />
+      <param name="quality" value="high" />
+      <param name="scale" value="noscale" />
+      <param NAME="FlashVars" value="text=#{text}">
+      <param name="bgcolor" value="#{bgcolor}">
+      <embed src="/flash/clippy.swf"
+             width="110"
+             height="12"
+             name="clippy"
+             quality="high"
+             allowScriptAccess="always"
+             type="application/x-shockwave-flash"
+             pluginspage="http://www.macromedia.com/go/getflashplayer"
+             FlashVars="text=#{text}"
+             bgcolor="#{bgcolor}"
+      />
+      </object>
+    EOF
+  end
 
 end
