ff86fa17d89edf305b49562355e2adf8c167d602
Dustin <dustin.webber@gmail.com>
2009-07-09 19:05:25 -0500

    fixed require issue with prawn

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,6 @@ Rails::Initializer.run do |config|
   config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
   config.gem "calendar_date_select"
   config.gem "prawn"
-  config.gem "gruff"
   config.gem "mattetti-googlecharts", :lib => "gchart", :source => "http://gems.github.com/"
   config.gem "ambethia-smtp-tls", :lib => "smtp-tls", :source => "http://gems.github.com/"
   config.gem "composite_primary_keys"
