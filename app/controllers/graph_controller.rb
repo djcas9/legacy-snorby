7e4e3fd468b1bd1ed07be6ff3415ab7c0f1e3e8b
Dustin <dustin.webber@gmail.com>
2009-07-12 17:18:30 -0500

    fix for x when > 10000

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,11 +1,16 @@
 class GraphController < ApplicationController
+  before_filter :get_event_counts
+
+
+  def get_event_counts
+    @high ||= Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 1']).size
+    @medium ||= Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 2']).size
+    @low ||= Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 3']).size
+    @all ||= @high+@medium+@low
+  end
 
   def event_severity
     title = Title.new("Event Severity")
-
-    high = Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 1']).size
-    medium = Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 2']).size
-    low = Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 3']).size
 
     data = []
 
@@ -15,9 +20,9 @@ class GraphController < ApplicationController
     pie.tooltip = '#val# Events of #total# Total.<br>#percent# of 100%<br>'
     pie.colours = ["#adffa2", "#f8f9a4", "#fb9c9c"]
 
-    data << PieValue.new(low, "Low Severity") unless low == 0
-    data << PieValue.new(medium, "Medium Severity") unless medium == 0
-    data << PieValue.new(high, "High Severity") unless high == 0
+    data << PieValue.new(@low, "Low Severity") unless @low == 0
+    data << PieValue.new(@medium, "Medium Severity") unless @medium == 0
+    data << PieValue.new(@high, "High Severity") unless @high == 0
 
     pie.values  = data
 
@@ -30,53 +35,20 @@ class GraphController < ApplicationController
 
     render :text => chart.to_s
   end
-
-  def sensor_information
-    @sensors ||= Sensor.all :include => [:events, :sensor]
-    data = []
-    title = Title.new("Sensor Information")
-
-    pie = Pie.new
-
-    for sensor in @sensors
-      data << PieValue.new(sensor.events.size, "#{sensor.hostname}") unless sensor.events.size <= 1
-    end
-
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#val# of #total#<br>#percent# of 100%'
-    pie.colours = ["#adffa2", "#f8f9a4", "#fb9c9c"]
-    pie.values  = data
-
-    chart = OpenFlashChart.new
-    chart.title = title
-    chart.set_bg_colour('#FFFFFF')
-    chart.add_element(pie)
-
-    chart.x_axis = nil
-
-    render :text => chart.to_s
-  end
-
 
   def category_information
-
-    high = Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 1']).size
-    medium = Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 2']).size
-    low = Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 3']).size
-
     data = []
     data_labels = []
-    unless high.blank?
-      data << high
+    unless @high.blank?
+      data << @high
       data_labels << "High Severity"
     end
-    unless medium.blank?
-      data << medium
+    unless @medium.blank?
+      data << @medium
       data_labels << "Medium Severity"
     end
-    unless low.blank?
-      data << low
+    unless @low.blank?
+      data << @low
       data_labels << "Low Severity"
     end
 
@@ -89,7 +61,7 @@ class GraphController < ApplicationController
 
     y = YAxis.new
     y.grid_colour= '#FFFFFF'
-    @all ||= Event.all.size
+    
     if @all >= 10000
       y.set_range(0, @all, 5000)
     else
