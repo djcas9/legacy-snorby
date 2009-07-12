034ce53f8a099e3add028bda84e127968d50b2cc
Dustin <dustin.webber@gmail.com>
2009-07-12 17:57:21 -0500

    css fix with filter button

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -50,7 +50,7 @@ class SearchesController < ApplicationController
 
   def auto_complete_for_search_keywords
     sig_name = params[:search][:keywords]
-    @events = Event.find(:all, :include => :sig, :group => 'sig.sig_name', :conditions => ['signature.sig_name LIKE ?', "%#{sig_name}%"])
+    @events = Event.find(:all, :include => :sig, :group => 'sig.sig_name', :conditions => ['signature.sig_name LIKE ?', "%#{sig_name}%"], :select => 'DISTINCT signature.sig_name')
     #@events = Event.find(:all, :include => :sig, :conditions => ['signature.sig_name LIKE ?', "%#{sig_name}%"], :select => 'DISTINCT sig_name')
     render :partial => 'keywords'
   end
