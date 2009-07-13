e26c2e2df317181d7a572a02eaf8ef958a5bd360
Dustin <dustin.webber@gmail.com>
2009-07-12 21:23:48 -0500

    moved all events for @events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,13 @@ class PagesController < ApplicationController
     @high ||= Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 1']).size
     @medium ||= Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 2']).size
     @low ||= Event.find(:all, :include => :sig, :conditions => ['signature.sig_priority = 3']).size
-    @all ||= @high+@medium+@low
+    @all ||= Event.all(:include => :sig)
   end
 
   def dashboard
     @g_event_severity ||= open_flash_chart_object(400,200, pie_event_severity_graph_url(:high => @high, :medium => @medium, :low => @low))
-    @g_category_information ||= open_flash_chart_object(400,200, bar_event_severity_graph_url(:high => @high, :medium => @medium, :low => @low, :all => @all))
-    @events ||= Event.all :include => :sig
+    @g_category_information ||= open_flash_chart_object(400,200, bar_event_severity_graph_url(:high => @high, :medium => @medium, :low => @low, :all => @all.size))
+    @events ||= @all
     @uniq_events ||= Event.all :group => 'signature'
     @uniq_adds ||= Iphdr.find(:all, :group => 'ip_src').uniq.size + Iphdr.find(:all, :group => 'ip_dst').uniq.size
     @categories ||= SigClass.all
