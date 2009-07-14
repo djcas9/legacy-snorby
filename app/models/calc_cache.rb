38e63de5c0cca3a43c9afef0f985390266bd2e53
Dustin <dustin.webber@gmail.com>
2009-07-14 17:58:23 -0500

    fix for example YAML spacing issue

--- app/models/calc_cache.rb
+++ app/models/calc_cache.rb
@@ -7,9 +7,28 @@ class CalcCache < ActiveRecord::Base
   end
   
   def self.rebuild_cache(options = {})
+    @events = Event.all(:include => [:sensor, :iphdr, {:sig => :sig_class }])
+    @events.each do |event|
+     cache_event = CalcCache.create(:name => event.sig.sig_name, 
+     :severity => event.sig.sig_priority, 
+     :date => event.timestamp, 
+     :hour => event.timestamp.hour, 
+     :count => "?")
+     cache_event.save!
+    end
   end
   
   def self.destroy_cache(options = {})
   end
   
 end
+
+
+# DB
+# t.string   "name"
+# t.integer  "severity"
+# t.string   "date"
+# t.integer  "hour"
+# t.integer  "count"
+# t.datetime "created_at"
+# t.datetime "updated_at"
