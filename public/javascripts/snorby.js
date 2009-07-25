32a794c57e73995c8541251da37a8ed551deee03
Dustin Webber <dustin.webber@gmail.com>
2009-07-24 21:08:36 -0500

    remove old global javascript for click functions

--- public/javascripts/snorby.js
+++ public/javascripts/snorby.js
@@ -1,10 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-function swap_in() {
-	Element.addClassName('more', 'loading');
-}
-
-function swap_out() {
-	Element.removeClassName('more', 'loading');
-}
