b51067d85eb084002849e65127492bb626fb0682
Dustin Webber <dustin.webber@gmail.com>
2009-07-26 17:30:52 -0500

    fixed xss issue (via emgent) fixed comment VP on update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :event, :foreign_key => [:sid, :cid]
   belongs_to :user
-  validates_presence_of :body, :on => :create, :message => "can't be blank"
+  validates_presence_of :body, :message => "Comment Can't be blank"
 end
