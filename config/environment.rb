ac148f312826ff4efc3dafa9e06667096ac1f862
Dustin Willis Webber <dustin.webber@gmail.com>
2009-10-15 18:36:40 -0500

    user page layout change

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'production'
 # Be sure to restart your server when you modify this file
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
-SNORBY_VERSION = '1.1.3' unless defined? SNORBY_VERSION
+SNORBY_VERSION = '2.0.0' unless defined? SNORBY_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
