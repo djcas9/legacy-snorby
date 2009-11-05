6034cc6e1773ea0c5a6924d38d41c113cf20daab
postmodern <postmodern.mod3@gmail.com>
2009-11-04 21:20:06 -0800

    Added a comment to UniqueEventsStats.

--- app/models/unique_events_stats.rb
+++ app/models/unique_events_stats.rb
@@ -2,6 +2,10 @@ require 'app/models/cached_stats'
 
 class UniqueEventsStats < CachedStats
 
+  #
+  # Calculates the statistics of unique events that have been previously
+  # seen.
+  #
   def calculate!
     super do
       events = Event.all(
