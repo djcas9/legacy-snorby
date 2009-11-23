480cfdecac56c21344b1961d1ea73c442f7cc4e3
postmodern <postmodern.mod3@gmail.com>
2009-11-22 23:09:57 -0800

    Added sid to the CachedStats model.

--- db/migrate/20091105041804_create_cached_stats.rb
+++ db/migrate/20091105041804_create_cached_stats.rb
@@ -4,8 +4,11 @@ class CreateCachedStats < ActiveRecord::Migration
       t.string :type
 
       t.string :name
+
+      t.integer :sid
       t.integer :start_cid, :default => 1
       t.integer :last_cid, :default => 0
+
       t.integer :statistic
       t.integer :expiration
 
